2972305380f4e90e47bfbc20659e4b2efa8a9c76
NhlawuBaloyi <baloyinhlawuleko3@gmail.com>
2023-10-27 20:55:15 +0200

    added cookie-parser

--- server.js
+++ server.js
@@ -2,8 +2,10 @@ const express = require("express");
 const app = express();
 const port = 3000;
 require("dotenv").config();
+const cookieParser = require("cookie-parser")
 
 app.use(express.json());
+app.use(cookieParser())
 
 app.use("/signup", require("./routes/signup"));
 app.use("/login", require("./routes/login"));
