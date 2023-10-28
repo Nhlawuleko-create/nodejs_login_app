78baef6dfa0c505eeb708b03006e638c8704258c
NhlawuBaloyi <baloyinhlawuleko3@gmail.com>
2023-10-28 07:50:16 +0200

    added token verification & employees route

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const app = express();
 const port = 3000;
 require("dotenv").config();
 const cookieParser = require("cookie-parser");
+const verifyToken = require("./middleware/verifyToken");
 
 app.use(express.json());
 app.use(cookieParser());
@@ -11,5 +12,8 @@ app.use("/signup", require("./routes/signup"));
 app.use("/login", require("./routes/login"));
 app.use("/refresh", require("./routes/refresh"));
 app.use("/logout", require("./routes/logout"));
+
+app.use(verifyToken);
+app.use("/employees", require("./routes/users"));
 
 app.listen(port, () => console.log(`Server is live on ${port}`));
